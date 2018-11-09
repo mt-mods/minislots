7c50cafb439288cee2f6dfcb2d4df80c0e718bb3
Vanessa Dannenberg <vanessa.e.dannenberg@gmail.com>
2018-11-09 18:41:14 -0500

    add warning messages in case someone "enables" the work files dir.

---
+++ minislots_work_files/init.lua
@@ -0,0 +1,3 @@
+print("[Minislots] Warning:  You \"enabled\" the minislots_work_files directory.")
+print("[Minislots]           Don't do that, it ain't a mod.")
+print("[Minislots]           It's just... work files. :-P")
